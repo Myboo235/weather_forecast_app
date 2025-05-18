92858290c1612108110156b108395efdf17e42da
Myboo235 <congnhuthy123@gmail.com>
2025-05-18 11:12:46 +0700

    feat: add forecast next 7 days using SARIMA and DLinear

--- model/dlinear/dlinear_forecast.py
+++ model/dlinear/dlinear_forecast.py
@@ -12,7 +12,7 @@ def dlinear_forecast():
     weather_df = load_weather_data("../../../data/weather_normalized.csv")
     model = DLinear(
         h=8,
-        input_size=24,
+        input_size=8,
         max_steps=1000,
         learning_rate=1e-3,
         val_check_steps=200,
@@ -31,49 +31,38 @@ def dlinear_forecast():
     return forecast_df
 
 def dlinear_forecast_next_7_days():
-
     weather_df = load_weather_data("../../../data/weather_normalized.csv")
+    
+    last_row = weather_df.tail(1)
+    last_time = last_row['ds'].iloc[0].time()
+    print(f"🕒 Last timestamp in data: {last_time}")
+    if last_time != pd.to_datetime("21:00").time():
+        print("⛔ Not last timestamp of the day. Stop predicting 7 days.")
+        return
 
+    print("✅ Last timestamp is 21:00. Proceed to predict next 7 days...")
+    model = DLinear(
+        h=56,  # 7 days * 8 steps/day
+        input_size=8,
+        max_steps=1000,
+        learning_rate=1e-3,
+        val_check_steps=200,
+        scaler_type='robust',
+    )
 
-    # Filter to the last row with time 21:00
-    last_21 = weather_df[weather_df['ds'].dt.time == pd.to_datetime("21:00").time()].iloc[-1]
+    nf = NeuralForecast(models=[model], freq='3h')
+    nf.fit(df=weather_df)
 
-    print(last_21)
-    # Extract the unique_id and timestamp of that last 21:00
-    # last_uid = last_21['unique_id']
-    # last_time = last_21['ds']
+    forecast_df = nf.predict()
+    forecast_df.to_csv("./dlinear/dlinear_forecast_next_7d.csv", index=False)
 
-    # # Get the 24 input steps ending at that time
-    # # cutoff_time = last_time
-    # # input_df = weather_df[
-    # #     (weather_df['unique_id'] == last_uid) &
-    # #     (weather_df['ds'] <= cutoff_time)
-    # # ].tail(24)  # last 24 steps for input
+    # Reverse normalization
+    forecast_reverse_normalized = inverse_predicted_z_scores(
+        forecast_df, 'DLinear', "../../../data/scaler.pkl"
+    )
+    forecast_reverse_normalized.to_csv("./dlinear/dlinear_forecast_next_7d_reversed.csv", index=False)
 
-    # # Initialize model
-    # model = DLinear(
-    #     h=56,  # 7 days * 8 steps/day
-    #     input_size=24,
-    #     max_steps=1000,
-    #     learning_rate=1e-3,
-    #     val_check_steps=200,
-    #     scaler_type='robust',
-    # )
-
-    # nf = NeuralForecast(models=[model], freq='3h')
-    # nf.fit(df=weather_df)  # Train on all available data
-
-    # # Forecast starting from the last 24 steps
-    # forecast_df = nf.predict(future_df=input_df)
-    # forecast_df.to_csv("./dlinear/dlinear_forecast_next_7d.csv", index=False)
-
-    # # Reverse normalization
-    # forecast_reverse_normalized = inverse_predicted_z_scores(
-    #     forecast_df, 'DLinear', "../../../data/scaler.pkl"
-    # )
-    # forecast_reverse_normalized.to_csv("./dlinear/dlinear_forecast_next_7d_reversed.csv", index=False)
-
-    # return forecast_reverse_normalized
+    return forecast_reverse_normalized
 
 dlinear_forecast()
-# dlinear_forecast_next_7_days()
+dlinear_forecast_next_7_days()
--- model/sarima/sarima_forecast.py
+++ model/sarima/sarima_forecast.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import pandas as pd
 from statsforecast import StatsForecast
 from statsforecast.models import ARIMA
 import json
@@ -39,5 +40,47 @@ def sarima_forecast():
 
     return forecast
 
-# # Test run
+def sarima_forecast_next_7_days():
+    weather_df = load_weather_data("../../../data/weather_normalized.csv")
+    
+    last_row = weather_df.tail(1)
+    last_time = last_row['ds'].iloc[0].time()
+    print(f"🕒 Last timestamp in data: {last_time}")
+    if last_time != pd.to_datetime("21:00").time():
+        print("⛔ Not last timestamp of the day. Stop predicting 7 days.")
+        return
+
+    print("✅ Last timestamp is 21:00. Proceed to predict next 7 days...")
+    with open('./sarima/sarima_parameters.json', 'r') as f:
+        params = json.load(f)
+
+    print("📦 Loaded ARIMA Parameters:")
+    print(f"🔢 Order: {params['order']}")
+    print(f"🌊 Seasonal Order: {params['seasonal_order']}")
+    print(f"📏 Season Length: {params['season_length']}")
+
+    sf = StatsForecast(
+        models=[
+            ARIMA(
+                order=tuple(params['order']),
+                seasonal_order=tuple(params['seasonal_order']),
+                season_length=params['season_length']
+            )
+        ],
+        freq='3h'
+    )
+
+    forecast = sf.forecast(df=weather_df, h=56, fitted=True)
+    forecast.to_csv("./sarima/sarima_forecast_next_7d.csv", index=False)
+
+    # Reverse normalization
+    forecast_reverse_normalized = inverse_predicted_z_scores(
+        forecast, 'ARIMA', "../../../data/scaler.pkl"
+    )
+    forecast_reverse_normalized.to_csv("./sarima/sarima_forecast_next_7d_reversed.csv", index=False)
+
+    return forecast_reverse_normalized
+
+
 sarima_forecast()
+sarima_forecast_next_7_days()
